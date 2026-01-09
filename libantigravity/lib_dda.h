40e595ac2c2f63f0b7a5f7ce579dba2d28648c38
Davit Kalantaryan <davit.kalantaryan@desy.de>
2026-01-09 19:55:47 +0100

    prepared everything to have library

--- libantigravity/lib_dda.h
+++ libantigravity/lib_dda.h
@@ -4,9 +4,7 @@
 #pragma once
 
 
-#include <QImage>
-
-typedef void (*TypeDesktopChange)(void*,const QImage*);
+typedef void (*TypeDesktopChange)(void*,const void*);
 
 int RegisterAndStartDesktopChangeCalbakc(void* a_userData, TypeDesktopChange a_clbk);
 void UnregisterDesktopChangeCalbakc(void);
