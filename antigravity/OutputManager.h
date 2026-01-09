52f665d10511a598bbbd5d3b4649a1d14afb6894
Davit Kalantaryan <davit.kalantaryan@desy.de>
2026-01-09 16:59:41 +0100

    deleted unnecessary function declaration in the class OUTPUTMANAGER

--- antigravity/OutputManager.h
+++ antigravity/OutputManager.h
@@ -34,7 +34,6 @@ class OUTPUTMANAGER
         DUPL_RETURN MakeRTV();
         void SetViewPort(UINT Width, UINT Height);
         DUPL_RETURN InitShaders();
-        DUPL_RETURN InitGeometry();
         DUPL_RETURN CreateSharedSurf(INT SingleOutput, _Out_ UINT* OutCount, _Out_ RECT* DeskBounds);
         DUPL_RETURN DrawFrame();
         DUPL_RETURN DrawMouse(_In_ PTR_INFO* PtrInfo);
